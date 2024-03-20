8f0c7de050d4c114f72ced2ecb7698a0f4f08802
MuznaFathima <muzuft@gmail.com>
2024-03-20 17:33:28 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,9 @@ const updateBlogByIdRoutes=require('./router/updateBlog')
 
 
 const app=express()
-const PORT=process.env.PORT || 5000
 
+
+const PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/signup',signupRouter)
